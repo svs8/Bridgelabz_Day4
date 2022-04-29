c26643956d88a7d069cc46a947c9e26b95e28a9e
svs8 <surakshithvittalshetty@gmail.com>
2022-04-29 16:26:07 +0530

    Added:Logic to check Options like No Play,Ladder or Snake

--- src/com/bridgelabz/snakeandladder/SnakeandLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeandLadder.java
@@ -1,12 +1,35 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeandLadder {
+    static int START_POSITION = 0;
+    static int PLAYERPOSITION = 0;
+    static final int  NO_PLAY=1;
+    static final int LADDER=2;
+    static final int SNAKE=3;
 
     private static int dieRoll(){
         int dieValue= (int)(Math.random()*10 %6)+1;
         return dieValue;
     }
-    static int START_POSITION = 0;
+    private static int getoption(){
+        int option= (int)(Math.random()*10 %3)+1;
+        return option;
+    }
+    private static int playgame(int option, int dieValue){
+        switch(option) {
+            case NO_PLAY:
+                System.out.println("No Play");
+                break;
+            case LADDER:
+                PLAYERPOSITION = PLAYERPOSITION + dieValue;
+                break;
+            case SNAKE:
+                PLAYERPOSITION = PLAYERPOSITION - dieValue;
+        }
+
+        return PLAYERPOSITION;
+        }
+
 
         public static void main(String[] args) {
             // welcome to snake ladder program.
@@ -15,6 +38,14 @@ public class SnakeandLadder {
 
             int dieValue= dieRoll();
             System.out.println("Die: "+dieValue);
+
+            int option= getoption();
+            System.out.println("option: "+option);
+
+            playgame(option,dieValue);
+
+            System.out.println("Playerposition is "+PLAYERPOSITION);
+
 
 
         }
