46f7c8a2fd410c1843f8c79fcd10c400943fab4f
svs8 <surakshithvittalshetty@gmail.com>
2022-04-29 15:21:40 +0530

    Added:First commit

---
+++ src/com/bridgelabz/snakeandladder/SnakeandLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeandLadder {
+
+        static int START_POSITION = 0;
+
+        public static void main(String[] args) {
+            // welcome to snake ladder program.
+            System.out.println("Welcome to snake ladder game");
+            System.out.println("Player will start from position "+START_POSITION);
+        }
+    }
